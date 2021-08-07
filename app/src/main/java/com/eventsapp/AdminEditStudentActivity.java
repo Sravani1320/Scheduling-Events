76d18c657d8a8ee99902e66da730734c0bbabdc6
rajesh <rajeshgurram163@gmail.com>
2021-08-07 13:19:02 -0400

    edit activity added

---
+++ app/src/main/java/com/eventsapp/AdminEditStudentActivity.java
@@ -0,0 +1,95 @@
+package com.eventsapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.eventsapp.api.ApiService;
+import com.eventsapp.api.RetroClient;
+import com.eventsapp.model.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AdminEditStudentActivity extends AppCompatActivity {
+    EditText etname, etemail, etphone, etpass;
+    Button btnupdatestudent;
+    TextView tvstudentlogin;
+    ProgressDialog progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edit_student);
+        getSupportActionBar().setTitle("Student Profile");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        etname =(EditText)findViewById(R.id.etname);
+        etemail = (EditText)findViewById(R.id.etemail);
+        etphone = (EditText)findViewById(R.id.etphone);
+        etpass = (EditText)findViewById(R.id.etpass);
+
+
+                etname.setText(getIntent().getStringExtra("name"));
+                etemail.setText(getIntent().getStringExtra("email"));
+                etphone.setText(getIntent().getStringExtra("phone"));
+                etpass.setText(getIntent().getStringExtra("pass"));
+
+        btnupdatestudent=(Button)findViewById(R.id.btnupdatestudent);
+        btnupdatestudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name=etname.getText().toString();
+                String email=etemail.getText().toString();
+                String phone=etphone.getText().toString();
+                String pass=etpass.getText().toString();
+
+                progress = new ProgressDialog(AdminEditStudentActivity.this);
+                progress.setMessage("updating please wait");
+                progress.show();
+                ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+                Call<ResponseData> call = service.studentupdateprofile(name,email,phone,pass);
+                call.enqueue(new Callback<ResponseData>() {
+                    @Override
+                    public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                        progress.dismiss();
+                        if (response.body().status.equals("true")) {
+                            Toast.makeText(AdminEditStudentActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(AdminEditStudentActivity.this, ManageStudentsActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(AdminEditStudentActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<ResponseData> call, Throwable t) {
+                        progress.dismiss();
+                        Toast.makeText(AdminEditStudentActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/eventsapp/AdminHomeActivity.java
+++ app/src/main/java/com/eventsapp/AdminHomeActivity.java
@@ -1,25 +1,37 @@
 package com.eventsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AdminHomeActivity extends AppCompatActivity {
 
     CardView cdevents,cdstudents,cdteachers,cdcomments;
+    Button btnlogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
+        btnlogout=(Button)findViewById(R.id.btnlogout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(AdminHomeActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
         cdevents=(CardView)findViewById(R.id.cdevents);
         cdevents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i=new Intent(AdminHomeActivity.this,AdminViewEventsActivity.class);
+                startActivity(i);
             }
         });
 
@@ -44,7 +56,8 @@ public class AdminHomeActivity extends AppCompatActivity {
         cdcomments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i=new Intent(AdminHomeActivity.this,AdminCommentsViewActivity.class);
+                startActivity(i);
             }
         });
 
